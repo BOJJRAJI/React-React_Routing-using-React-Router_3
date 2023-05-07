7cdb97f5e71a7202c2a8a5f30c086de0b97efad3
raji <bojjaraji@gmail.com>
2023-05-07 12:04:24 +0000

    Solution

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,71 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoading: true}
+
+  componentDidMount() {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    console.log(response)
+    const data = await response.json()
+    const updateData = {
+      id: data.id,
+      title: data.title,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      topic: data.topic,
+      content: data.content,
+    }
+
+    this.setState({blogData: updateData, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData, isLoading} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        {isLoading ? (
+          <Loader
+            type="TailSpin"
+            height={50}
+            width={50}
+            color="#00BFFF"
+            className="spinner"
+          />
+        ) : (
+          <div className="blog-item">
+            <h2 className="blog-details-title">{title}</h2>
+
+            <div className="author-details">
+              <img className="author-pic" src={avatarUrl} alt={author} />
+              <p className="details-author-name">{author}</p>
+            </div>
+
+            <img className="blog-image" src={imageUrl} alt={title} />
+            <p className="blog-content">{content}</p>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
